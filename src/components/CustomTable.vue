bdf40a619ecd4987e3d902c964be624f423d4fc6
liwk <liwk@dpark.com.cn>
2020-07-24 09:34:49 +0800

    表格可传入props

--- src/components/CustomTable.vue
+++ src/components/CustomTable.vue
@@ -1,13 +1,9 @@
 <template>
   <div>
     <div style="font-size: 12px;display: flex;justify-content: space-between; background-color: lightsteelblue;">
-      <div class="table-head-item">项目</div>
-      <div class="table-head-item">排名</div>
-      <div class="table-head-item">当日考勤人数</div>
-      <div class="table-head-item">月考勤合格工程数</div>
-      <div class="table-head-item">月考勤合格率</div>
+      <div v-for="columnName in columnData" class="table-head-item">{{columnName}}</div>
     </div>
-    <div v-for="(item,index) in records" :style="index%2===1?drawLineStyle:normalLineStyle"
+    <div v-for="(item,index) in rowData" :style="index%2===1?drawLineStyle:normalLineStyle"
          :key="item.name">
       <div class="table-head-item">{{item.name}}</div>
       <div class="table-head-item">{{item.rank}}</div>
@@ -21,39 +17,14 @@
 <script>
   export default {
     name: 'Table',
+    props: {
+      columnData: Array,
+      rowData: Array
+    },
     data () {
       return {
-        records: [
-          {
-            name: '项目1',
-            rank: '1',
-            checkNums: '265',
-            qualifiedNums: '25',
-            percent: '23.8%'
-          },
-          {
-            name: '项目2',
-            rank: '2',
-            checkNums: '265',
-            qualifiedNums: '25',
-            percent: '23.8%'
-          },
-          {
-            name: '项目3',
-            rank: '3',
-            checkNums: '265',
-            qualifiedNums: '25',
-            percent: '23.8%'
-          }, {
-            name: '项目4',
-            rank: '4',
-            checkNums: '265',
-            qualifiedNums: '25',
-            percent: '23.8%'
-          }
-        ],
-        drawLineStyle:'font-size: 12px;display: flex;justify-content: space-between;background-color:lightcyan',
-        normalLineStyle:'font-size: 12px;display: flex;justify-content: space-between;',
+        drawLineStyle: 'font-size: 12px;display: flex;justify-content: space-between;background-color:lightcyan',
+        normalLineStyle: 'font-size: 12px;display: flex;justify-content: space-between;',
       }
     }
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
                     :unqualified-date="unqualifiedDate" :qualified-info="qualifiedMessage"
                     :unqualified-info="unqualifiedMessage"
                     @monthChanged="monthChange" :key="currentDate.month"/>
-    <customtabel></customtabel>
+    <customtabel :column-data="columnData" :row-data="rowData"></customtabel>
   </div>
 </template>
 
@@ -22,6 +22,34 @@
     },
     data () {
       return {
+        columnData:['项目','排名','当日考勤人数','月考勤合格工程数','月考勤合格率'],
+        rowData:[{
+          name: '项目1',
+          rank: '1',
+          checkNums: '265',
+          qualifiedNums: '25',
+          percent: '23.8%'
+        },
+          {
+            name: '项目2',
+            rank: '2',
+            checkNums: '265',
+            qualifiedNums: '25',
+            percent: '23.8%'
+          },
+          {
+            name: '项目3',
+            rank: '3',
+            checkNums: '265',
+            qualifiedNums: '25',
+            percent: '23.8%'
+          }, {
+            name: '项目4',
+            rank: '4',
+            checkNums: '265',
+            qualifiedNums: '25',
+            percent: '23.8%'
+          }],
         currentDate: {
           year: new Date().getFullYear(),
           month: new Date().getMonth(),
